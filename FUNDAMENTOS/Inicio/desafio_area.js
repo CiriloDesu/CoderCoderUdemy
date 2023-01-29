ac4f00fed81c3120525cf7ff3277d57beb950c30
CiriloDesu <32491548+CiriloDesu@users.noreply.github.com>
2023-01-29 19:40:21 -0300

    inicio

---
+++ FUNDAMENTOS/Inicio/desafio_area.js
@@ -0,0 +1,6 @@
+// PI * raio * raio
+
+let raio = 10
+let pi = 3.14
+let calc = pi * (raio * raio)
+console.log(calc)
---
+++ FUNDAMENTOS/Inicio/desafio_troca.js
@@ -0,0 +1,10 @@
+let a = 75
+let b = 45
+
+/*let temp = a
+a = b
+b = temp*/
+
+
+console.log(a)
+console.log(b)
